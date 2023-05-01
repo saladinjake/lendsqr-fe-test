d0b029d937c0db10fb5ea42a962b1d1f8a18d01c
victorjuwa <victor.juwa@kuda.com>
2023-05-02 00:07:07 +0100

    user detail flesh up ui skin component

--- src/components/shared/library/components/RowsAndColumns-v1/Row.tsx
+++ src/components/shared/library/components/RowsAndColumns-v1/Row.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
 
-const Row = ({ children }) => {
-  return <div className="row">{children}</div>;
+const Row = ({ children, style={} }) => {
+  return <div  style={{...style}} className="row">{children}</div>;
 };
 
 export default Row;
