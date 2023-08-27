a0200f59bd997ae29fd580a264d86e0f0042e026
victorjuwa <victor.juwa@kuda.com>
2023-08-27 18:23:57 +0100

    mock up routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-
 function App() {
 	return (
 		<div>
-			<>GET THIS JOB DONE</>
+			<Routes>
+				<Route path="/" element={<>Login</>} />
+				<Route path="/login" element={<>Login</>} />
+				<Route path="/dashboard/*" element={<>Dashboard</>}>
+					<Route index element={<>Users</>} />
+					<Route index path="users" element={<>Users page</>} />
+					<Route path="users/:id/profile" element={<>user details</>} />
+				</Route>
+
+				<Route path="*" element={<h1>PAGE NOT FOUND</h1>} />
+			</Routes>
 		</div>
 	);
 }
